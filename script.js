0bca84710ba0b1cb7bbbf82f806d866ed692c22f
Kashima Takumi <kodokurider@gmail.com>
2014-05-13 01:07:46 +0900

    add websocket test

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ var elementAlpha = document.querySelector("#alpha");
 var elementBeta = document.querySelector("#beta");
 var elementGamma = document.querySelector("#gamma");
 var elementTime = document.querySelector("#time");
+var elementStatus = document.querySelector("#status");
 
 var prevTime = new Date();
+
+var ws = new WebSocket('ws://192.168.0.3:3000');
 
 window.addEventListener("deviceorientation", function(e) {
     var a = e.alpha;
@@ -20,4 +23,19 @@ window.addEventListener("deviceorientation", function(e) {
     elementGamma.textContent = "g: " + g;
     elementTime.textContent  = "t(ms): " + diffTime;
 
+    // ws.send(b);
+
 }, false);
+
+ws.onopen = function(e) {
+   console.log('connected'); 
+   elementStatus.textContent = 'connected';
+}
+
+ws.onmessage = function(e) {
+    console.log('received message: ' + e.data);
+}
+
+ws.onclose = function(e) {
+    console.log('connection released!');
+}
